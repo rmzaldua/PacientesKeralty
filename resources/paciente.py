fec8129ca91e310f4f453179c8d77645a1c8b927
rmzaldua <70669784+rmzaldua@users.noreply.github.com>
2020-10-28 14:30:23 -0500

    Update paciente.py

--- resources/paciente.py
+++ resources/paciente.py
@@ -1,19 +1,19 @@
 from flask import Response, request
-from database.models import Client
+from database.models import Paciente
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_restful import Resource
 
 
-class ClientsApi(Resource):
+class PacientesApi(Resource):
     def get(self):
-        clients = Client.objects().to_json()
-        return Response(clients, mimetype="application/json", status=200)
+        pacientes = Paciente.objects().to_json()
+        return Response(pacientes, mimetype="application/json", status=200)
 
     @jwt_required
     def post(self):
         body = request.get_json()
-        client = Client(**body).save()
-        id = client.id
+        paciente = Paciente(**body).save()
+        id = paciente.id
         return {'id': str(id)}, 200
 
 
@@ -21,17 +21,17 @@ class ClientApi(Resource):
     @jwt_required
     def put(self, id):
         body = request.get_json()
-        Client.objects.get(id=id).update(**body)
+        Paciente.objects.get(id=id).update(**body)
         return '', 200
 
     @jwt_required
     def delete(self, id):
-        client = Client.objects.get(id=id).delete()
+        paciente = Paciente.objects.get(id=id).delete()
         return '', 200
 
     def get(self, id):
         try:
-            client = Client.objects.get(id=id).to_json()
-            return Response(client, mimetype="application/json", status=200)
+            paciente = Paciente.objects.get(id=id).to_json()
+            return Response(paciente, mimetype="application/json", status=200)
         except Exception as error:
             return Response(error, status=400, mimetype='application/json')
