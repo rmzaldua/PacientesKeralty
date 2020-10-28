f5db2008986ec0a38e2f83217631669741fc15e3
rmzaldua <70669784+rmzaldua@users.noreply.github.com>
2020-10-28 14:26:47 -0500

    Update routes.py

--- resources/routes.py
+++ resources/routes.py
@@ -1,7 +1,9 @@
 from .client import ClientsApi, ClientApi
+from .paciente import PacientesApi, PacienteApi
 
 
 def initialize_routes(api):
     api.add_resource(ClientsApi, '/api/clients')
     api.add_resource(ClientApi, '/api/client/<id>')
-
+    api.add_resource(PacienteApi, '/api/pacientes')
+    api.add_resource(ClientApi, '/api/paciente/<id>')
