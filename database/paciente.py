3e424729df71a70cf0a1dea5321e8d57bdefd052
rmzaldua <70669784+rmzaldua@users.noreply.github.com>
2020-10-29 06:53:11 -0500

    Create paciente.py

---
+++ database/paciente.py
@@ -0,0 +1,7 @@
+from .db import db
+
+class Paciente(db.Document):
+    doc_id = db.StringField(required=True, unique=True)
+    name = db.StringField(required=True)
+    last_name = db.StringField(required=True)
+    preexistence = db.ListField(db.StringField(), required=True)
