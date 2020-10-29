2630aa34c9a946cc475acace69a0e8cf9708e228
rmzaldua <70669784+rmzaldua@users.noreply.github.com>
2020-10-29 06:55:26 -0500

    Update paciente.py

--- resources/paciente.py
+++ resources/paciente.py
@@ -1,37 +1,14 @@
 from flask import Response, request
-from database.models import Paciente
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from database.paciente import Paciente
 from flask_restful import Resource
 
-
-class PacientesApi(Resource):
+class PacienteApi(Resource):
     def get(self):
-        pacientes = Paciente.objects().to_json()
-        return Response(pacientes, mimetype="application/json", status=200)
+        clients = Paciente.objects().to_json()
+        return Response(clients, mimetype="application/json", status=200)
 
-    @jwt_required
     def post(self):
         body = request.get_json()
-        paciente = Paciente(**body).save()
-        id = paciente.id
+        client = Paciente(**body).save()
+        id = client.id
         return {'id': str(id)}, 200
-
-
-class ClientApi(Resource):
-    @jwt_required
-    def put(self, id):
-        body = request.get_json()
-        Paciente.objects.get(id=id).update(**body)
-        return '', 200
-
-    @jwt_required
-    def delete(self, id):
-        paciente = Paciente.objects.get(id=id).delete()
-        return '', 200
-
-    def get(self, id):
-        try:
-            paciente = Paciente.objects.get(id=id).to_json()
-            return Response(paciente, mimetype="application/json", status=200)
-        except Exception as error:
-            return Response(error, status=400, mimetype='application/json')
